2d61f2e1a0a840811339fd1c716f26fefd22400d
Thomas Ragonha Rivera <thomas.rivera@lmvidros.local>
2018-01-22 17:02:20 -0300

    SWDice

---
+++ SWDice/SWDice/Dice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWDice
+{
+    class Dice
+    {
+        //Fields
+        private int sides;
+
+        //Properties
+        public int Sides
+        {
+            get
+            {
+                return sides;
+            }
+
+            set
+            {
+                    sides = value;
+            }
+        }
+
+        //Constructors
+
+        public Dice(int sides)
+        {
+            this.Sides = sides;
+        }
+    }
+}
---
+++ SWDice/SWDice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sides;
+            if (int.TryParse(Console.ReadLine(), out sides))
+            {
+                Dice dice = new Dice(sides);
+                Console.WriteLine(dice.Sides);
+            }
+            else
+            {
+                Console.WriteLine("Inválido!");
+            }
+
+            Console.Read();
+        }
+    }
+}
